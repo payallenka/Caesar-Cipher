5213c83d66005b4ae152fc572d7f3c3e9656d665
payallenka <83643171+payallenka@users.noreply.github.com>
2023-07-18 16:43:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,82 +2,25 @@ def encrypt():
     a = int(input("enter the integer you wish to shift the letters by:"))
     b = input("enter the text to be encrypted")
     l = []
-    d = dict()
+    s = ""
     for i in b.lower():
         l.append(i)
-    d[0]="a"
-    d[1]="b"
-    d[2]="c"
-    d[3]="d"
-    d[4]="e"
-    d[5]="f"
-    d[6]="g"
-    d[7]="h"
-    d[8]="i"
-    d[9]="j"
-    d[10]="k"
-    d[11]="l"
-    d[12]="m"
-    d[13]="n"
-    d[14]="o"
-    d[15]="p"
-    d[16]="q"
-    d[17]="r"
-    d[18]="s"
-    d[19]="t"
-    d[20]="u"
-    d[21]="v"
-    d[22]="w"
-    d[23]="x"
-    d[24]="y"
-    d[25] = "z"
-    g=""
-    for k in range(len(l)):
-        for j in d:
-            if l[k]==d[j]:
-                g=g+(d[j+a])
-    print(g)
+    for i in range(len(l)):
+        s+= (chr(ord(l[i])+a))
+
+    print(s)
 
 def decryp():
     a = int(input("enter the integer by which the message was shifted:"))
     b = input("enter the text to be encrypted")
     l = []
-    d = dict()
+    s = ""
     for i in b.lower():
         l.append(i)
-    d[0] = "a"
-    d[1] = "b"
-    d[2] = "c"
-    d[3] = "d"
-    d[4] = "e"
-    d[5] = "f"
-    d[6] = "g"
-    d[7] = "h"
-    d[8] = "i"
-    d[9] = "j"
-    d[10] = "k"
-    d[11] = "l"
-    d[12] = "m"
-    d[13] = "n"
-    d[14] = "o"
-    d[15] = "p"
-    d[16] = "q"
-    d[17] = "r"
-    d[18] = "s"
-    d[19] = "t"
-    d[20] = "u"
-    d[21] = "v"
-    d[22] = "w"
-    d[23] = "x"
-    d[24] = "y"
-    d[25] = "z"
-    g = ""
-    for k in range(len(l)):
-        for j in d:
-            if l[k] == d[j]:
-                g = g + (d[j - a])
+    for i in range(len(l)):
+        s += (chr(ord(l[i]) - a))
 
-    print(g)
+    print(s)
 
 
 print("menu")
@@ -88,19 +31,3 @@ if ask==1:
     encrypt()
 elif ask==2:
     decryp()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
